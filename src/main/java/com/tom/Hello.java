9f934f08437b9037f5c9608791ae62492e875786
Evelyn <reset920215@gmail.com>
2022-06-10 17:31:17 +0800

    Hello world with kotlin

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -2,6 +2,8 @@ package com.tom;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!" );
+       // System.out.println("Hello!" );
+        Person p = new Person();
+        p.greeting();
     }
 }
